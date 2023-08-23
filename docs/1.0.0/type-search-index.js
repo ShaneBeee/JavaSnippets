2d46f6f860d8d6cfbe9f7c862f3c4a5b8fb9bcae
ShaneBee <shanebolenback@me.com>
2023-08-23 11:34:34 -0700

    add docs

---
+++ docs/1.0.0/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.shanebeestudios.snippets.gui","l":"GUI"},{"p":"com.shanebeestudios.snippets.gui","l":"GUI.InventoryData"}];updateSearchResults();
